627029efcd426a846ad9257f44722d07c7c05160
Devansh Bansal <devanshb7860@gmail.com>
2025-01-23 23:59:58 +0530

    celebrity solution addded

---
+++ F_StacksQueues/Q12_celebrityProblem.java
@@ -0,0 +1,40 @@
+package F_StacksQueues;
+
+import java.util.Stack;
+
+public class Q12_celebrityProblem {
+    
+    public static void main(String[] args) {
+        int[][] arr = {{0,1,1,1} , 
+                       {1,0,1,0} , 
+                       {0,0,0,0,} , 
+                        {1,1,1,0}};
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i<arr.length;i++){
+            st.push(i);
+        }
+
+        while(st.size()>=2){
+            int i = st.pop();
+            int j = st.pop();
+
+            if(arr[i][j] == 1){
+                st.push(j);
+            }
+            else{
+                st.push(i);
+            }
+
+        }
+        int pot = st.peek();
+        for(int i = 0; i<arr.length; i++){
+            if(i != pot){
+                if(arr[i][pot] == 0 || arr[pot][i] == 1){
+                    System.out.println("No celebrity");
+                    return;
+                }
+            }
+        }
+        System.out.println(pot);
+    }
+}
