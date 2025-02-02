b766fbe503814bdd458204d1e8409055b65272e1
Devansh Bansal <devanshb7860@gmail.com>
2025-02-02 21:57:09 +0530

    ceil floor sol updated

--- D_Arrays/Q11_ceilFloor.java
+++ D_Arrays/Q11_ceilFloor.java
@@ -2,7 +2,7 @@ package D_Arrays;
 import java.util.*;
 public class Q11_ceilFloor {
     // we are required to print the ceil and floor of a given element in an array.
-    // 
+    // given array should be sorted
     
     public static void main(String[] args) {
         int[] arr = {10,20,30,40,50,60,70,80,90,100};
@@ -26,10 +26,28 @@ public class Q11_ceilFloor {
             else if (n<arr[mid]) {
                 hi = mid-1;
                 floor = arr[mid];
+                if(n>arr[hi]){
+                    ceil = arr[hi];
+                    break;
+                }  
+                else if(n == arr[hi]){
+                    ceil = arr[hi];
+                    floor = arr[hi];
+                    break; 
+                } 
             }
             else if (n>arr[mid]) {
                 lo = mid+1;
                 ceil = arr[mid];
+                if(n<arr[lo]){
+                    floor = arr[lo];
+                    break;
+                }
+                else if(n == arr[lo]){
+                   ceil = arr[lo];
+                   floor = arr[lo];
+                    break; 
+                }
             }
         }
         System.out.println(ceil + "," + floor);
