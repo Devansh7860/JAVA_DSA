723811c71d789fd35cc77eefde89faac5d99f2c1
Devansh Bansal <devanshb7860@gmail.com>
2025-02-12 01:03:44 +0530

    remove duplicates in sorted list sol added

---
+++ G_LinkedList/Q17_removeDuplicatesInSortedLL.java
@@ -0,0 +1,37 @@
+package G_LinkedList;
+
+public class Q17_removeDuplicatesInSortedLL {
+    // Remove duplicates in a sorted linked list
+
+    // SOLVED ON LEETCODE
+    public class ListNode {
+        ListNode next;
+        int val;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode fs = head;
+
+        while (fs != null && fs.next != null) {
+            if (fs.val == fs.next.val) {
+                fs.next = fs.next.next;
+            } else {
+                fs = fs.next;
+            }
+        }
+        return head;
+    }
+
+}
