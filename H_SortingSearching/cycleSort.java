4a343af5bb5fa35db7dd037e431b9247819ee893
Devansh Bansal <devanshb7860@gmail.com>
2025-02-26 19:03:08 +0530

    cycle sort algorithm added

--- H_SortingSearching/cycleSort.java
+++ H_SortingSearching/cycleSort.java
@@ -3,6 +3,8 @@ package H_SortingSearching;
 public class cycleSort {
     // Cyclic Sort Algorithm
     // used when elements are in continuous range from 1 to n or 0 to n
+    // tc = O(n)
+    // sc = O(1)
 
     public static void cycle(int[] arr){
         for(int i = 0; i<arr.length; i++){
@@ -14,7 +16,7 @@ public class cycleSort {
         }
 
     }
-    
+      
     public static void main(String[] args) {
         int[] arr = {3,5,1,2,4};
         cycle(arr);
